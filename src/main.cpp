851987dab15c418e24e2f700f6168d800aa65a82
Frederic Taillandier <frederic@auterion.com>
2023-07-19 11:05:29 +0200

    feat: base-proj (#1)

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/return5.h"
+
+auto main(int argc, char** argv) -> int {
+    std::cout << "hello world\n";
+    return 0;
+}
---
+++ src/return5.cpp
@@ -0,0 +1,6 @@
+#include "../include/return5.h"
+
+// Only used for testing purposes
+auto return5() -> int {
+    return 5;
+}
